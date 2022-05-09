97fbb9af858d27f7ae059ace353fed774f9402fb
9alaty-coL <trantanloc@gmail.com>
2022-05-09 13:29:35 +0700

    add redirect for login

--- source/routes/index.js
+++ source/routes/index.js
@@ -2,7 +2,7 @@ const authRoute = require('./auth')
 const roomRoute = require('./room')
 
 const route = app => {
-    app.get('/', (res, req)=>{
+    app.get('/', (req, res)=>{
         res.send("Welcome to hotelmanagement api")
     })
     app.use('/auth', authRoute)
