fedc50bb6ad75b0da7f880556de277d20a9fc76a
DoriTran <tranquocdong308@gmail.com>
2022-05-13 10:50:59 +0700

    New Dong Commit for testing

--- source/routes/index.js
+++ source/routes/index.js
@@ -3,7 +3,7 @@ const roomRoute = require('./room')
 
 const route = app => {
     app.get('/', (req, res)=>{
-        res.send("Welcome to hotelmanagement api. Deployed by heroku - :>")
+        res.send("Welcome to hotelmanagement api. Deployed by heroku - :> :> :>")
     })
     app.use('/auth', authRoute)
     app.use('/room', roomRoute)
