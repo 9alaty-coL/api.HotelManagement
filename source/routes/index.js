a9b2ddf7176af4e2462d31ee2da8437a884be63d
9alaty-coL <trantanloc@gmail.com>
2022-05-23 16:43:22 +0700

    merge

--- source/routes/index.js
+++ source/routes/index.js
@@ -3,11 +3,7 @@ const roomRoute = require('./room')
 
 const route = app => {
     app.get('/', (req, res)=>{
-<<<<<<< HEAD
         res.send("Welcome to hotelmanagement api. Deployed by heroku. Version 12")
-=======
-        res.send("Welcome to hotelmanagement api. Deployed by heroku (Dong deployed)")
->>>>>>> bc5db6f5beaa4df81f75374cbcac9fb35410c21f
     })
     app.use('/auth', authRoute)
     app.use('/room', roomRoute)
