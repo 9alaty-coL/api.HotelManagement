259d3e663b877f27f7fd0cf1cf9de7cc792e67ce
Tr-nT-nL-c <66482009+9alaty-coL@users.noreply.github.com>
2022-10-25 20:25:15 +0700

    Remove redundant line

--- source/index.js
+++ source/index.js
@@ -4,7 +4,6 @@ const express = require("express")
 const app = express()
 const port = process.env.PORT || 8000;
 
-
 // app.all('/', function(req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "*");
 //   res.header("Access-Control-Allow-Headers", "X-Requested-With");
